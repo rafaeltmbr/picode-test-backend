57b16c52394fdc920393d30cb6b76aae407eef29
Rafhael Payão <51467263+rafaeltmbr@users.noreply.github.com>
2021-02-25 23:37:35 -0300

    feat: GET /books route implemented

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,14 +1,23 @@
 const Book = require("../models/Book");
 class BooksController {
-  index(req, res) {
-    res.json({ message: "index route" });
+  async index(req, res) {
+    try {
+      const tag = req.query.tag || "";
+      const books = await Book.find({ tags: { $regex: new RegExp(tag, "i") } }).select(
+        "-_id id title author description pages tags"
+      );
+
+      res.json(books);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   }
 
-  create(req, res) {
+  async create(req, res) {
     res.json({ message: "create route" });
   }
 
-  delete(req, res) {
+  async delete(req, res) {
     res.json({ message: "delete route" });
   }
 }
